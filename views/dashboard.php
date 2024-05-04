37cf047a742ee9e79a3baa9c87d9d2b785fda5c8
田口雅彦 <masahiko@taguchimasahikonoMacBook-Air.local>
2024-05-04 10:36:30 +0900

    Session full_name to username

--- views/dashboard.php
+++ views/dashboard.php
@@ -30,7 +30,7 @@ $all_users = $user->getAllUsers();
        
 
             <div class="navbar-nav">
-                <span class="navbar-text"><?= $_SESSION['full_name'] ?></span>
+                <span class="navbar-text"><?= $_SESSION['username'] ?></span>
                 <form action="../actions/logout.php" method="post" class="d-flex ms-2">
                     <button type="submit" class="text-danger bg-transparent border-0">
                         Logout
